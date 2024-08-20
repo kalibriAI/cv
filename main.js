3cb16558455c48b53a0fc2052ef6938d8f5f35f9
Kalibri AI <feet.eex@gmail.com>
2024-08-20 23:18:35 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,71 @@
+// Объект с уровнями для каждой технологии
+const techLevels = {
+  Python: 'Middle',
+  TypeScript: 'Junior',
+  JavaScript: 'Junior',
+  Django: 'Junior',
+  Aiogram: 'Middle',
+  React: 'Junior',
+  Figma: 'Junior',
+  Bootstrap: 'Middle',
+  Tailwind: 'Junior',
+  PostgreSQL: 'Junior',
+  GIT: 'Junior',
+  SQL: 'Middle',
+  HTML: 'Middle',
+  CSS: 'Middle'
+};
+
+// Функция для обновления текста и стиля кнопки при нажатии
+function toggleButtonState(button) {
+  const originalText = button.getAttribute('data-original-text');
+  
+  if (button.classList.contains('face')) {
+    button.classList.remove('face');
+    button.classList.add('back');
+    // Обновление текста при переходе на 'back'
+    if (techLevels[originalText]) {
+      button.textContent = techLevels[originalText];
+    }
+  } else {
+    button.classList.remove('back');
+    button.classList.add('face');
+    
+    // Возврат оригинального текста при переходе на 'face'
+    button.textContent = originalText;
+  }
+}
+
+// Получение всех кнопок и установка оригинального текста
+const allButtons = document.querySelectorAll('.item'); 
+
+allButtons.forEach(button => {
+  // Убедимся, что кнопка имеет текст, который присутствует в techLevels
+  const text = button.textContent.trim();
+  if (techLevels[text]) {
+    // Сохраняем оригинальный текст в data-атрибут
+    button.setAttribute('data-original-text', text);
+    
+    // Добавляем обработчик клика
+    button.addEventListener('click', function() {
+      toggleButtonState(button);
+    });
+  }
+});
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const cursor = document.createElement('div');
+    cursor.classList.add('cursor');
+    document.body.appendChild(cursor);
+  
+    document.addEventListener('mousemove', (e) => {
+      cursor.style.left = `${e.pageX}px`;
+      cursor.style.top = `${e.pageY}px`;
+    });
+  });
+  
